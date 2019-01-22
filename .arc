3c2fb3973621b8c15d8abccd16fedc7781e118f6
Brian LeRoux <b@brian.io>
2019-01-21 20:23:51 -0800

    show most basic setup

--- .arc
+++ .arc
@@ -3,7 +3,8 @@ test-ws
 
 @ws
 # no further config required
+# client code is in /public
+# serverless code is in /src/ws/*
 
 @http
 get /
-get /js/:file
--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,18 +1,15 @@
 exports.handler = async function http(req) {
+  // todo: add deployment web socket url to page as global
   return {
     type: 'text/html; charset=utf8',
-    body: render()
+    body: `<!doctype html>
+<html>
+<body>
+<h1>Web sockets</h1>
+<main>Loading...</main>
+<input id=message type=text placeholder="Enter message" autofocus>
+<script type=module src=/index.mjs></script>
+</body>
+</html>`
   }
-}
-
-function render() {
-  return `<!doctype html>
-  <html>
-  <body>
-  <h1>Web sockets</h1>
-  <main>Loading...</main>
-  <input id=message type=text placeholder="Enter message" autofocus>
-  <script type=module src=/js/index.mjs></script>
-  </body>
-  </html>`
 }
